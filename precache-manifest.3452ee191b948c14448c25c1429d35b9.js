077a3ee10b015fc6b9a0e7c6fb3beda8cca56965
Pegasus <aniscima@gmail.com>
2020-07-30 15:04:31 +0530

    Updates

---
+++ precache-manifest.3452ee191b948c14448c25c1429d35b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d2d51dacbe5b889ea4f4ca41f092da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6053a05c5249a7514c88",
+    "url": "/static/css/main.7b06d06b.chunk.css"
+  },
+  {
+    "revision": "5c071528591b58f58f4e",
+    "url": "/static/js/2.b7f13db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7f13db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6053a05c5249a7514c88",
+    "url": "/static/js/main.e7b7efa4.chunk.js"
+  },
+  {
+    "revision": "65711f9c67d9bf78601d",
+    "url": "/static/js/runtime-main.da79d3a2.js"
+  }
+]);
